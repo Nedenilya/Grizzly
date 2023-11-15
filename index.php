e3ad05047f4915fe553a4218520637fd9673975e
Nedenilya <nedenil27@gmail.com>
2023-11-15 12:16:46 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/app.css">
+	<title>Grizzly Digital</title>
+</head>
+<body>
+	<div class="content">
+		
+	</div>
+</body>
+<script type="text/javascript" src="scripts/app.js"></script>
+</html>
