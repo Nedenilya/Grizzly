efd949b13882c903e4b379d6ef6b97d7ef31096b
Nedenilya <nedenil27@gmail.com>
2023-11-17 20:12:22 +0300

    added slider for mobile layout

--- index.php
+++ index.php
@@ -12,19 +12,19 @@
 	<div class="container">
 		<div class="row mt-3">
 			<div class="input-group mb-3">
-			    <input type="text" class="form-control" placeholder="Phone number" aria-label="Phone number">
+			    <input type="text" class="form-control phone" placeholder="Phone number" aria-label="Phone number">
 			    <div class="input-group-append">
-			        <button class="btn btn-outline-secondary" type="button">Define</button>
+			        <button class="btn btn-outline-secondary define" type="button">Define</button>
 			    </div>
 			</div>
 		</div>
 	</div>
-	<div class="container">
+	<div class="container banner">
 		<p class="row title title_text mb-5">
 			Korzyści ze współpracy z nami
 		</p>
-		<div class="row mb-4">
-			<div class="col p-1">
+		<div class="row mb-4 gy-2">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i1.png" alt="">
 					<div class="text-desc">
@@ -35,7 +35,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i2.png" alt="">
 					<div class="text-desc">
@@ -46,7 +46,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i3.png" alt="">
 					<div class="text-desc">
@@ -57,7 +57,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i4.png" alt="">
 					<div class="text-desc">
@@ -70,7 +70,7 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i5.png" alt="">
 					<div class="text-desc">
@@ -81,7 +81,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i6.png" alt="">
 					<div class="text-desc">
@@ -92,7 +92,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i7.png" alt="">
 					<div class="text-desc">
@@ -103,7 +103,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col p-1">
+			<div class="col-lg-3 p-1">
 				<div class="block p-4">
 					<img class="mt-2" src="icons/i8.png" alt="">
 					<div class="text-desc">
@@ -116,7 +116,117 @@
 			</div>
 		</div>
 	</div>
+
+
+	<div id="carousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="slide-btn active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="1" class="slide-btn" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="2" class="slide-btn" aria-label="Slide 3"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="3" class="slide-btn" aria-label="Slide 4"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="4" class="slide-btn" aria-label="Slide 5"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="5" class="slide-btn" aria-label="Slide 6"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="6" class="slide-btn" aria-label="Slide 7"></button>
+			<button type="button" data-bs-target="#carousel" data-bs-slide-to="7" class="slide-btn" aria-label="Slide 8"></button>
+		</div>
+		<p class="row title title_text title_mob mb-5">
+			Korzyści ze współpracy z nami
+		</p>
+		<div class="carousel-inner p-4">
+			<div class="carousel-item active" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i1.png" alt="">
+					<div class="text-desc">
+						Przechowywanie towarów ponadgabarytowych
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i2.png" alt="">
+					<div class="text-desc">
+						Elastyczne warunki współpracy
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i3.png" alt="">
+					<div class="text-desc">
+						Integracja i zarządzanie zamówieniami
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i4.png" alt="">
+					<div class="text-desc">
+						Wysyłka zamówień w dniu kompletacji
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i5.png" alt="">
+					<div class="text-desc">
+						Niskie koszty dostawy
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i6.png" alt="">
+					<div class="text-desc">
+						Gwarancja bezpieczeństwa towarów
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i7.png" alt="">
+					<div class="text-desc">
+						Całodobowy wideo monitoring
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item" data-bs-interval="3000">
+				<div class="block">
+					<img class="mt-2" src="icons/i8.png" alt="">
+					<div class="text-desc">
+						Wysyłka zamówień do różnych krajów
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="scripts/jquery-3.7.1.min.js"></script>
 <script type="text/javascript" src="scripts/app.js"></script>
 </html>
