6753400dd63a8ebef19d038bbe526352a9e1f455
Nedenilya <nedenil27@gmail.com>
2023-11-17 20:55:06 +0300

    link cookie lib

--- index.php
+++ index.php
@@ -228,5 +228,6 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="scripts/jquery-3.7.1.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.cookies.js" ></script>
 <script type="text/javascript" src="scripts/app.js"></script>
 </html>
