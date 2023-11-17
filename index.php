add80c5fc5f59609342ae2f7df5c2c3843d6c196
Nedenilya <nedenil27@gmail.com>
2023-11-17 17:08:44 +0300

    template layout

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<title>Grizzly Digital</title>
 </head>
 <body>
-	<div class="container-md">
+	<div class="container">
 		<div class="row mt-3">
 			<div class="input-group mb-3">
 			    <input type="text" class="form-control" placeholder="Phone number" aria-label="Phone number">
@@ -18,8 +18,102 @@
 			    </div>
 			</div>
 		</div>
+	</div>
+	<div class="container">
+		<p class="row title title_text mb-5">
+			Korzyści ze współpracy z nami
+		</p>
+		<div class="row mb-4">
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i1.png" alt="">
+					<div class="text-desc">
+						Przechowywanie towarów ponadgabarytowych
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i2.png" alt="">
+					<div class="text-desc">
+						Elastyczne warunki współpracy
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i3.png" alt="">
+					<div class="text-desc">
+						Integracja i zarządzanie zamówieniami
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i4.png" alt="">
+					<div class="text-desc">
+						Wysyłka zamówień w dniu kompletacji
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="row">
-
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i5.png" alt="">
+					<div class="text-desc">
+						Niskie koszty dostawy
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i6.png" alt="">
+					<div class="text-desc">
+						Gwarancja bezpieczeństwa towarów
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i7.png" alt="">
+					<div class="text-desc">
+						Całodobowy wideo monitoring
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
+			<div class="col p-1">
+				<div class="block p-4">
+					<img class="mt-2" src="icons/i8.png" alt="">
+					<div class="text-desc">
+						Wysyłka zamówień do różnych krajów
+					</div>
+					<div class="opis">
+						Opis
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
